1ec666f83a7c3a9a0d77457c59edac78ddbb4e92
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-09 19:03:19 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,12 @@
+class PigLatinizer 
+  attr_accessor :text
+  def initialize(string)
+    @text = string 
+  end 
+  def pigged 
+    original = @text 
+    first_letter = original[0]
+    inter = original[1..-1]
+    final = inter + first_letter + 'ay'
+    final 
+  end     
