bc23c76f9c4029487a49b6b42a0cfba72141a083
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-09 19:03:24 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,14 @@
-class PigLatinizer 
+class PigLatinizer
   attr_accessor :text
   def initialize(string)
-    @text = string 
-  end 
-  def pigged 
-    original = @text 
+    @text = string
+  end
+  def pigged
+    original = @text
     first_letter = original[0]
     inter = original[1..-1]
     final = inter + first_letter + 'ay'
-    final 
-  end     
+    final
+  end
+
+end
