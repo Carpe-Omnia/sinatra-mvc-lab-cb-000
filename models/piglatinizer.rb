afa0cf09283012cf8685e440addf5fbbc2c24e0e
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-09 19:47:38 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,15 @@
 class PigLatinizer
   def piglatinize(text)
-    original = text.split(' ')
-    first_letter = original[0]
-    inter = original[1..-1]
-    final = inter + first_letter + 'ay'
-    final
+    ray = []
+    final_text = ""
+    split = text.split(' ')
+    split.each do |word|
+      ray << turn_word(word)
+    end 
+    ray.each do |word|
+      final_text += "#{word} "
+    end 
+    final_text     
   end
   def turn_word(word)
     turnt = [""]
